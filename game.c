60254b7162aef6d156b59af25bda2091b788c421
Trevor Klingler <trklingler3@gmail.com>
2021-06-18 11:44:42 -0600

    Chance and Community Chest cards primal push
    all movement  from cards is functional. Some money systems remain unimplemented.

--- game.c
+++ game.c
@@ -22,6 +22,12 @@ void *play_monopoly(void *results_lock) {
         struct tile_t properties[40];
         memcpy(properties, default_properties, sizeof(default_properties));
         
+        struct card_t chance[16];
+        memcpy(chance, default_chance, sizeof(default_chance));
+
+        struct card_t chest[16];
+        memcpy(chest, default_chest, sizeof(default_chest));
+
         int round;
         for (round = 0; round < MAX_ROUNDS_PER_GAME; round++) {
             int player;
@@ -45,6 +51,26 @@ void *play_monopoly(void *results_lock) {
                     if (players[player].position == 30) {
                         players[player].position = 10;
                         tile_landings[10]++;
+                    }
+
+                    if (strcmp(properties[players[player].position].tile_name, "Chance") == 0) {
+                        int prev_position = players[player].position;
+                        
+                        struct card_t chance_card = draw_card(chance);
+                        chance_action(&players[player], chance_card);
+
+                        if (prev_position != players[player].position) {
+                            tile_landings[players[player].position]++;
+                        }
+                    } else if (strcmp(properties[players[player].position].tile_name, "Community Chest") == 0) {
+                        int prev_position = players[player].position;
+                        
+                        struct card_t chest_card = draw_card(chest);
+                        chest_action(&players[player], chest_card);
+
+                        if (prev_position != players[player].position) {
+                            tile_landings[players[player].position]++;
+                        }
                     }
                 } while (roll.doubles);
                 
