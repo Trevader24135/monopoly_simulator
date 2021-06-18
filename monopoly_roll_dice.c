522b5d71f38d4112a480747b0689c7a265a1b049
Trevor Klingler <trklingler3@gmail.com>
2021-06-18 11:36:14 -0600

    Dice roll 1-6 instead of 0-5

--- monopoly_roll_dice.c
+++ monopoly_roll_dice.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 struct dice_t roll_dice(void) {
-    int die_one = rand() % 6;
-    int die_two = rand() % 6;
+    int die_one = rand() % 6 + 1;
+    int die_two = rand() % 6 + 1;
     struct dice_t roll = {die_one + die_two + 2, die_one == die_two};
     return roll;
 }
